518cf32d382435d380759f997daa620e669c4c8d
Erin <erindrichmond@gmail.com>
2016-05-04 20:33:46 -0400

    updated homepage

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".about").click(
+    function(){
+      $('#aboutSubMenu').toggle();
+    }); 
+});
+// $('.about').hover(function(){
+//   $('#aboutSubMenu').removeClass('hide');
+//   $('#aboutSubMenu').addClass('show');
+// });
