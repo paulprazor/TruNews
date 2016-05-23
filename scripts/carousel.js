6f96fa351f70e148294372960fc8913fbbc9ca64
Erin <erindrichmond@gmail.com>
2016-05-23 12:00:34 -0400

    landing page

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -8,7 +8,7 @@
                     width = carousel.innerWidth();
 
                 if (width >= 600) {
-                    width = width / 4;
+                    width = width / 4.3;
                 } else if (width >= 350) {
                     width = width / 2;
                 }
